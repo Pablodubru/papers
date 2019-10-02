028ac6b392aea20ed7d59d900ee13404458dfc3f
HumaSOFT <humasoft@ing.uc3m.es>
2019-10-02 19:43:54 +0200

    modificado:     main/ol-pos-360.cpp

--- main/ol-pos-360.cpp
+++ main/ol-pos-360.cpp
@@ -20,22 +20,22 @@ int main(){
 
 
     //--Can port communications--
-    SocketCanPort pm31("can1");
-    SocketCanPort pm32("can1");
-    SocketCanPort pm33("can1");
+    SocketCanPort pm1("can1");
+    SocketCanPort pm2("can1");
+    SocketCanPort pm3("can1");
 
-    CiA402SetupData sd31(2048,24,0.001, 0.144);
-    CiA402SetupData sd32(2048,24,0.001, 0.144);
-    CiA402SetupData sd33(2048,24,0.001, 0.144);
+    CiA402SetupData sd1(2*2048,3.7,0.001, 1.1); //max amp 10.1
+    CiA402SetupData sd2(2*2048,3.7,0.001, 1.1);//max amp 10.1
+    CiA402SetupData sd3(2*2048,3.7,0.001, 1.1);//max amp 10.1
 
-    CiA402Device m31 (31, &pm31, &sd31);
-    CiA402Device m32 (32, &pm32, &sd32);
-    CiA402Device m33 (33, &pm33, &sd33);
+    CiA402Device m1 (1, &pm1, &sd1);
+    CiA402Device m2 (2, &pm2, &sd2);
+    CiA402Device m3 (3, &pm3, &sd3);
 
     //--Neck Kinematics--
     double l0=0.1085;
     double lg0=l0+0.003;
-    double radio=0.0075;
+    double radio=0.01;
     GeoInkinematics neck_ik(0.052,0.052,l0); //kinematics geometric
     vector<double> lengths(3);
     double targetAngle1, targetAngle2, targetAngle3;
@@ -43,20 +43,20 @@ int main(){
     sleep(4); //wait to imu sensor
 
     //    Motor setup
-    m31.Reset();
-    m31.SwitchOn();
+    m1.Reset();
+    m1.SwitchOn();
 
-    m32.Reset();
-    m32.SwitchOn();
+    m2.Reset();
+    m2.SwitchOn();
 
-    m33.Reset();
-    m33.SwitchOn();
+    m3.Reset();
+    m3.SwitchOn();
 
 
     //set velocity and aceleration (rads/s)
-    m31.SetupPositionMode(10,10);
-    m32.SetupPositionMode(10,10);
-    m33.SetupPositionMode(10,10);
+    m1.SetupPositionMode(10,10);
+    m2.SetupPositionMode(10,10);
+    m3.SetupPositionMode(10,10);
 
     //
 
@@ -77,9 +77,9 @@ int main(){
     targetAngle2=(lg0-lengths[1])/radio;//*180/(0.01*M_PI);
     targetAngle3=(lg0-lengths[2])/radio;//*180/(0.01*M_PI);
 
-    m31.SetPosition(targetAngle1);
-    m32.SetPosition(targetAngle2);
-    m33.SetPosition(targetAngle3);
+    m1.SetPosition(targetAngle1);
+    m2.SetPosition(targetAngle2);
+    m3.SetPosition(targetAngle3);
 
 
     for (double t=0;t<2*dts;t+=dts)
@@ -88,13 +88,13 @@ int main(){
         cout <<"target1: "<<targetAngle1;
         cout <<", target2: "<<targetAngle2;
         cout <<", target3: "<<targetAngle3<<endl;
-        cout <<"pos1:    "<<m31.GetPosition();
-        cout <<", pos2: "<<m32.GetPosition();
-        cout <<", pos3: "<<m33.GetPosition()<<endl;
+        cout <<"pos1:    "<<m1.GetPosition();
+        cout <<", pos2: "<<m2.GetPosition();
+        cout <<", pos3: "<<m3.GetPosition()<<endl;
         cout << "orient: "<<orient<<" incl: "<<incli<<endl;
         tilt.readSensor(incSensor,oriSensor);
 //        cout << "incli_sen: " << incSensor << " , orient_sen: " << oriSensor << endl;
-        graph << t << " , " << targetAngle1 << " , " << m31.GetPosition() << " , " << targetAngle2 << " , " << m32.GetPosition() << " , " << targetAngle3 << " , " << m33.GetPosition() << " , " << incli << " , " << incSensor << " , " << orient << " , " << oriSensor <<endl;
+        graph << t << " , " << targetAngle1 << " , " << m1.GetPosition() << " , " << targetAngle2 << " , " << m2.GetPosition() << " , " << targetAngle3 << " , " << m3.GetPosition() << " , " << incli << " , " << incSensor << " , " << orient << " , " << oriSensor <<endl;
 
         Ts.WaitSamplingTime();
 
@@ -110,9 +110,9 @@ int main(){
     //}
 
     sleep(1);
-        m31.SetPosition(0);
-        m32.SetPosition(0);
-        m33.SetPosition(0);
+        m1.SetPosition(0);
+        m2.SetPosition(0);
+        m3.SetPosition(0);
         sleep (1);
 
 }
