663e544fa63976e1fe1d5afa971d2a10e01f186c
humasoft <humasoft@humasoft.ing.uc3m.es>
2020-02-11 19:53:34 +0100

    modificado:     main/iros2020-identification.cpp 	graficas/Iros2020-Identification/RLSData.csv

--- main/iros2020-identification.cpp
+++ main/iros2020-identification.cpp
@@ -68,11 +68,53 @@ void capturaDatos(){
 
 }
 
+void moveincl(double Inclination,SerialArduino& ArduinoSensor,CiA402Device& Motor, ofstream& WriteFile,ofstream& WriteFile2, double samplingTime){
+
+
+    ///--identification--
+    ///
+    long numOrder=2, denOrder=2;
+    vector<double>numerator,denominator;
+    OnlineSystemIdentification Gident(numOrder,denOrder);
+
+    SamplingTime Ts(samplingTime);
+    double psr = 0.0, cs= 0;
+    double incSensor,oriSensor;
+    if (ArduinoSensor.readSensor(incSensor,oriSensor) <0){}
+    for(double t=samplingTime;t<1000;t=t+samplingTime){
+
+         psr=0.01*((rand() % 10)-5);
+         cs = Inclination+psr-incSensor;
+         Gident.UpdateSystem(cs,incSensor);
+         Motor.SetVelocity(cs);
+         WriteFile<<t<<","<<Inclination+psr-incSensor<<",";
+         cout<<t<<","<<Inclination+psr-incSensor<<",";
+         Gident.GetZTransferFunction(numerator,denominator);
+         for (auto &num : numerator){WriteFile2<<num<<",";}
+         for (auto &den : denominator){WriteFile2<<den<<",";}
+         WriteFile2<<endl;
+
+         Ts.WaitSamplingTime();
+
+         if (ArduinoSensor.readSensor(incSensor,oriSensor) <0){}
+         WriteFile<<Motor.GetPosition()<<","<<Motor.GetVelocity()<<","<<Motor.GetAmps()<<","<<incSensor<<endl;
+         cout<<Motor.GetPosition()<<","<<Motor.GetVelocity()<<","<<Motor.GetAmps()<<","<<incSensor<<endl;
+
+
+
+     }
+
+     Motor.SetVelocity(0);
+     Motor.SwitchOff();
+}
+
 int main(int argc, char *argv[])
 {
+
+
+
     ///--sensor tilt--
     SerialArduino tilt;
-    double incSensor,oriSensor;
 
     //m2
     SocketCanPort pm2("can1");
@@ -83,37 +125,8 @@ int main(int argc, char *argv[])
     m2.Setup_Velocity_Mode(200,200);
 
     ofstream data("/home/humasoft/code/papers/graficas/Iros2020-Identification/RLSData.csv",std::ofstream::out);
+    ofstream data2("/home/humasoft/code/papers/graficas/Iros2020-Identification/RLSPOL.csv",std::ofstream::out);
 
-
-
-    double dts=0.02;
-    SamplingTime Ts(dts);
-
-    double psr = 0.0, isignal = 0.0;
-
-
-
-    for(double t=dts;t<1000;t=t+dts)
-    {
-        // sinusoidal + pseudorandom
-        psr = 0.1*((rand() % 10)-5);
-        isignal=0;
-           m2.SetPosition(isignal+psr);
-        cout << "t: "<< t << ", pos: " << +3*sin(5*t) << endl;
-        Ts.WaitSamplingTime();
-        if (tilt.readSensor(incSensor,oriSensor) <0){}
-
-        cout<<"Read position: "<<m2.GetPosition()<<", vel: "<<m2.GetVelocity()
-            <<" and those amps:"<<m2.GetAmps()<<endl;
-
-        cout << "Inc: " << incSensor << " ; Ori: "  << oriSensor << endl;
-
-        data << t << ", "  << isignal+psr << ", "<< m2.GetPosition() <<", "<< m2.GetVelocity()
-             <<", "<< m2.GetAmps() <<", "<<  incSensor << ", " << oriSensor << endl;
-
-
-    }
-     m2.SwitchOff();
-    return 0;
-
+    double InC=15;
+    moveincl(InC,tilt,m2,data,data2,0.02);
 }
