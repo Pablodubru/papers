1d2113aa36a23b6cfd8cd1ee5290ffb279504a41
munozyanez <munoz.yanez@gmail.com>
2020-05-07 19:00:03 +0200

    modificado:     README.md 	modificado:     lib/fcontrol 	modificado:     main/ieeeaccess-adaptive.cpp

--- main/ieeeaccess-adaptive.cpp
+++ main/ieeeaccess-adaptive.cpp
@@ -14,36 +14,45 @@
 #include "Kinematics.h"
 
 
-// Demo Close loop with Inclination Sensor, steps 20º incl - 0..45º orientation.
+/// IEEE Access paper experimental code
+/// Performs all experiments used in the paper and
+/// stores all data in .csv files
+
 // It requires: -Platform inclination=0
 //              -Reset IMU sensor
 
 
-int main ()
-{
-  //--sensor--
-  SerialArduino tilt;
-  double incSensor,oriSensor;
+int main (){
+
+//--sensor--
+SerialArduino imu;
+double imuIncli,imuOrien;
 //    sleep(4); //wait for sensor
 
-  ofstream data("/home/humasoft/code/papers/graficas/test2/ids900.csv",std::ofstream::out);
+ofstream data("/home/humasoft/code/papers/graficas/test2/ids900.csv",std::ofstream::out);
 
-  //Samplinfg time
-  double dts=0.025;
-  SamplingTime Ts(dts);
+//Samplinfg time
+double dts=0.025; //
+SamplingTime Ts(dts);
 
-  //tau = 0.1
+/// System identification
+//tau = 0.1
 //    0.09516
 //   ----------
 //   z - 0.9048
-  SystemBlock filter(0.09516,0,- 0.9048,1);
+SystemBlock filter(0.09516,0,- 0.9048,1);
+ulong numOrder=1,denOrder=2;
+OnlineSystemIdentification model(numOrder, denOrder, filter, 0.98, 0.8 );
+vector<double> num(numOrder+1),den(denOrder+1); //(order 0 also counts)
+SystemBlock sys(num,den); //the resulting identification
 
-  ulong numOrder=0,denOrder=1;
-  OnlineSystemIdentification model(numOrder, denOrder);//, filter, 0.98, 0.8 );
-  SystemBlock sys;
-  FPDBlock con(1,1,1,dts);
-  PIDBlock intcon(0.1,0.01,0,dts);
-  double phi,mag,w=1;
+
+
+///Controller and tuning
+FPDBlock con(1,1,-0.5,dts);
+FPDTuner tuner ( 60, 1, dts);
+PIDBlock intcon(0.1,0.01,0,dts);
+//double phi,mag,w=1;
 
 //  data << "Controller PID" << " , " << " 0.1,0.05,0,dts "<< endl;
 
@@ -79,128 +88,121 @@ int main ()
 //  m3.Setup_Torque_Mode();
 
 
-
-
-  IPlot id;
+IPlot id;
 
 
 
-  //tilt initialization
-  for (double t=0; t<6; t+=dts)
-  {
-  if (tilt.readSensor(incSensor,oriSensor)>=0) break;
+//tilt sensor initialization
+for (double t=0; t<6; t+=dts)
+{
+    if (imu.readSensor(imuIncli,imuOrien)>=0) break;
 
-  }
+}
 
 
 //  m1.SetTorque(0.01);
 //  m2.SetTorque(0.01);
 //  m3.SetTorque(0.01);
-  double torque,incli,orien;
-
-  vector<double> num(numOrder+1),den(denOrder+1); //(order 0 also counts)
-
-
-  //only for position mode.
-  //--Neck Kinematics--
-  double l0=0.1085;
-  double lg0=l0+0.002;
-  double radius=0.01; //winch radius
-  GeoInkinematics neck_ik(0.052,0.052,l0); //kinematics geometric
-  vector<double> lengths(3);
-
-  double targetAngle1, targetAngle2, targetAngle3;
 
 
 
-  //populate system matrices
-  double interval=10; //in seconds
-  for (double t=0;t<interval; t+=dts)
-  {
-      incli=0.01*((rand() % 10 + 1)-5);
-      orien=0;
+////only for position mode.
+////--Neck Kinematics--
+//double l0=0.1085;
+//double lg0=l0+0.002;
+//double radius=0.01; //winch radius
+//GeoInkinematics neck_ik(0.052,0.052,l0); //kinematics geometric
+//vector<double> lengths(3);
 
-      neck_ik.GetIK(incli,orien,lengths);
-      targetAngle1=(lg0-lengths[0])/radius;//*180/(0.01*M_PI);
-      targetAngle2=(lg0-lengths[1])/radius;//*180/(0.01*M_PI);
-      targetAngle3=(lg0-lengths[2])/radius;//*180/(0.01*M_PI);
+//double targetAngle1, targetAngle2, targetAngle3;
 
-      m1.SetPosition(targetAngle1);
-      m2.SetPosition(targetAngle2);
-      m3.SetPosition(targetAngle3);
+double psr=+0.01*((rand() % 10 + 1)-5); //pseudorandom
 
-      if (tilt.readSensor(incSensor,oriSensor) <0)
-      {
-          cout << "Sensor error! " ;
-          //Due to sensor error set motors zero velocity.
-          cout << "Inc: " << incSensor << " ; Ori: "  << oriSensor << endl;
+//populate system matrices
+double interval=2; //in seconds
+for (double t=0;t<interval; t+=dts)
+{
 
-      }
+    psr=+0.01*((rand() % 10 + 1)-5); //pseudorandom
 
-      model.UpdateSystem(incli, incSensor);
+    if (imu.readSensor(imuIncli,imuOrien) <0)
+    {
+        cout << "Sensor error! " ;
+        //Sensor error, do nothing.
+        cout << "Inc: " << imuIncli << " ; Ori: "  << imuOrien << endl;
 
-      Ts.WaitSamplingTime();
+    }
+
+    model.UpdateSystem(psr, imuIncli);
+    model.GetSystemBlock(sys);
+    tuner.TuneIsom(sys,con);
+
+    Ts.WaitSamplingTime();
+
+}
+
+double incli=15, orien=0, error=0, cs=0;
+
+interval=10; //in seconds
+for (double t=0;t<interval; t+=dts)
+{
+
+    psr=+0.01*((rand() % 10 + 1)-5); //new pseudorandom data
+
+//    incli=15+psr;
+//    orien=0;
+
+    ///read sensor
+    if (imu.readSensor(imuIncli,imuOrien) <0)
+    {
+        cout << "Sensor error! ";
+        //Sensor error, do nothing.
+        cout << "Inc: " << imuIncli << " ; Ori: "  << imuOrien << endl;
+    }
+
+    //Compute error
+    error=incli-imuIncli;
+
+    //Controller command
+    cs = error > con;
+    m1.SetPosition(cs);
+
+    //Update model
+    model.UpdateSystem(cs, imuIncli);
+    model.GetSystemBlock(sys);
+    sys.GetZTransferFunction(num,den);
+
+    //Update controller
+    tuner.TuneIsom(sys,con);
 
 
-  }
+    data << t;
+    for (int i=num.size()-1; i>=0; i--)
+    {
+        data << ", " << num[i];
+    }
+    //      cout << "],[ " << idDen.back();
+    for (int i=den.size()-1; i>=0; i--)
+    {
+        data << ", " << den[i];
+
+    }
+    data << endl;
+    Ts.WaitSamplingTime();
 
 
-  interval=10; //in seconds
-  for (double t=0;t<interval; t+=dts)
-  {
+}
 
-      incli=(2*t)+0.01*((rand() % 10 + 1)-5);
-      orien=0;
-
-      neck_ik.GetIK(incli,orien,lengths);
-      targetAngle1=(lg0-lengths[0])/radius;//*180/(0.01*M_PI);
-      targetAngle2=(lg0-lengths[1])/radius;//*180/(0.01*M_PI);
-      targetAngle3=(lg0-lengths[2])/radius;//*180/(0.01*M_PI);
-
-      m1.SetPosition(targetAngle1);
-      m2.SetPosition(targetAngle2);
-      m3.SetPosition(targetAngle3);
-
-      if (tilt.readSensor(incSensor,oriSensor) <0)
-      {
-          cout << "Sensor error! ";
-          //Due to sensor error set motors zero velocity.
-          cout << "Inc: " << incSensor << " ; Ori: "  << oriSensor << endl;
-
-      }
-
-          model.UpdateSystem(incli, incSensor);
-          cout << "Inc: " << incSensor << " ; Ori: "  << oriSensor << endl;
-
-
-//      model.PrintZTransferFunction(dts);
-      model.GetZTransferFunction(num,den);
-      Ts.WaitSamplingTime();
-
-//      cout << "matlab G=tf([ " << idNum.back() ;
-      data << t;
-      for (int i=num.size()-1; i>=0; i--)
-      {
-          data << ", " << num[i];
-      }
-//      cout << "],[ " << idDen.back();
-      for (int i=den.size()-1; i>=0; i--)
-      {
-          data << ", " << den[i];
-
-      }
-      data << endl;
-  }
-
+//  m1.SetPosition(0);
+//  m2.SetPosition(0);
+//  m3.SetPosition(0);
+//  sleep (1);
   m1.SetPosition(0);
   m2.SetPosition(0);
   m3.SetPosition(0);
-  sleep (1);
-  m1.SetPosition(0);
-  m2.SetPosition(0);
-  m3.SetPosition(0);
+
   sleep (3);
-  data.close();
+data.close();
 
 
 
